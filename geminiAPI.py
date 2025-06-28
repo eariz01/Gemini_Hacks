693010c88fa15bd03a8f053190a8204b411c0167
Emilio Aristizabal <earistizabal102006@gmail.com>
2025-06-28 14:23:08 -0400

    Gemini API Function

---
+++ geminiAPI.py
@@ -0,0 +1,21 @@
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyA1HK9fRuEOryDmNLgAxE_McNJ23W84Exg")
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+def classify_data_sentiment(headline: str, date: str = None):
+  prompt = f'Classify the Sentiment of the following Headline as Positive, Negative, or Neutral and give a brief reason as to why:\n\n"{headline}"'
+
+  response = model.generate_content(prompt)
+
+  return{
+    "headline" : headline,
+    "Date Posted" : date,
+    "gemini_response": response.text
+  }
+
+test_result = classify_data_sentiment("Amazon shares a drop after a weak holiday forecast\n")
+print(test_result)
+
+                           
+  
